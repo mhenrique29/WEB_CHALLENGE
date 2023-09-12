894b51d52f9f2dd88b3c5f4267fc83f2c075f1ef
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-11 22:52:02 -0300

    Adicionado componente Route

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      
+      <Route>
         <Route path="/" element={<Home/>} />
-
+      </Route>
       
     </BrowserRouter>
   </React.StrictMode>
