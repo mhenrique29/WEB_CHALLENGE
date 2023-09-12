e0672748e7c62843a673f46ff7e8c7aabeae24e4
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-11 22:38:10 -0300

    Tech Splinter - ajustes componentes

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
+      
         <Route path="/" element={<Home/>} />
 
-      </Routes>
+      
     </BrowserRouter>
   </React.StrictMode>
 );
